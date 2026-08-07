8bbf82a496d211fed3aa9da390f4b49ec290a0a6
anthrpmrphc <anthrpmrphc@gmail.com>
2026-08-08 03:28:16 +1200

    Rewinding features enforcing table data validity with regards to neighbourhood membership in neighbourhood_groups.

--- src/main/lib/schema/listing_schema.py
+++ src/main/lib/schema/listing_schema.py
@@ -4,69 +4,61 @@ Internal-Pandas-Dataframe-Handling Schema
 compatibility: Inside Airbnb listings-summary V.2 (exclusive)
 """
 
-import pandas as pd
-
 from pandera.pandas import DataFrameModel, Field, dataframe_check
 from datetime import datetime
 
-import pandera.typing as ptp
+from typing import Optional
 
-import json
+#NOTE: this is definitely too much for now, I will keep it here commented just in case
 
-DISTRICT_WARD_HIERARCHY: list
+# import pandas as pd
+# import pandera.typing as ptp
+# import json
 
-with open("src/main/lib/schema/neighbourhoods_heirarchy.json", encoding="utf-8") as neighbourhoods_heirarchy:
-    DISTRICT_WARD_HIERARCHY = json.load(neighbourhoods_heirarchy)
+# DISTRICT_WARD_HIERARCHY: list
+
+# with open("src/main/lib/schema/neighbourhoods_heirarchy.json", encoding="utf-8") as neighbourhoods_heirarchy:
+#     DISTRICT_WARD_HIERARCHY = json.load(neighbourhoods_heirarchy)
 
 # listings summary v.2 schema
 
+#NOTE: this is definitely too much for now, I will keep it here commented just in case
+
 class ListingSchema(DataFrameModel):
-    id                              : int       = Field(gt=0) 
-    name                            : str                     
-    host_id                         : int       = Field(gt=0, nullable=True) 
-    host_name                       : str       = Field(nullable=True)
-    neighbourhood_group             : str       
-    neighbourhood                   : str       
-    latitude                        : float     = Field(ge=-90, le=90) 
-    longitude                       : float     = Field(ge=-180, le=180) 
-    room_type                       : str       = Field(isin=["Private room", "Entire home/apt", "Shared room", "Hotel room"]) 
-    price                           : float     = Field(ge=0, nullable=True)
-    minimum_nights                  : int       = Field(ge=1, nullable=True) 
-    number_of_reviews               : int       = Field(ge=0) 
-    last_review                     : datetime  = Field(nullable=True)
-    reviews_per_month               : float     = Field(gt=0, nullable=True)
-    calculated_host_listings_count  : int       = Field(ge=1, nullable=True) 
-    availability_365                : int       = Field(ge=0) 
-    number_of_reviews_ltm           : int       = Field(ge=0) 
-    license                         : str       = Field(nullable=True) 
+    id                              : Optional[int]       = Field(gt = 0) 
+    name                            : Optional[str]                     
+    host_id                         : Optional[int]       = Field(nullable = True, gt = 0, ) 
+    host_name                       : Optional[str]       = Field(nullable = True)
+    neighbourhood_group             : Optional[str]       
+    neighbourhood                   : Optional[str]       
+    latitude                        : Optional[float]     = Field(ge = -90, le = 90) 
+    longitude                       : Optional[float]     = Field(ge = -180, le = 180) 
+    room_type                       : Optional[str]       = Field(isin = ["Private room", "Entire home/apt", "Shared room", "Hotel room"]) 
+    price                           : Optional[float]     = Field(nullable = True, ge = 0)
+    minimum_nights                  : Optional[int]       = Field(nullable = True, ge = 1) 
+    number_of_reviews               : Optional[int]       = Field(ge = 0) 
+    last_review                     : Optional[datetime]  = Field(nullable = True)
+    reviews_per_month               : Optional[float]     = Field(nullable = True, gt = 0)
+    calculated_host_listings_count  : Optional[int]       = Field(nullable = True, ge = 1) 
+    availability_365                : Optional[int]       = Field(ge = 0) 
+    number_of_reviews_ltm           : Optional[int]       = Field(ge = 0) 
+    license                         : Optional[str]       = Field(nullable = True) 
     # month_year                      : DatetimeIndex # TODO: reconfigure row-wise formatting to ISO-8601
 
-    @dataframe_check
-    def valid_neighbour_heirarchy(
-        cls,
-        data_frame: pd.DataFrame,
-        name = "valid_neighbour_heirarchy") -> ptp.Series[bool]:
+    #NOTE: this is definitely too much for now, I will keep it here commented just in case
 
-        """
-        check that ward belongs in district
-        """
-        return data_frame.apply(
-            lambda row: row["neighbourhood"] in DISTRICT_WARD_HIERARCHY.get(row["neighbourhood_group"], []),
-            axis = 1
-        )
+    # @dataframe_check
+    # def valid_neighbour_heirarchy(
+    #     cls,
+    #     data_frame: pd.DataFrame,
+    #     name = "valid_neighbour_heirarchy") -> ptp.Series[bool]:
 
-if __name__ == "__main__":
-    """example usage"""
+    #     """
+    #     check that ward belongs in district
+    #     """
+    #     return data_frame.apply(
+    #         lambda row: row["neighbourhood"] in DISTRICT_WARD_HIERARCHY.get(row["neighbourhood_group"], []),
+    #         axis = 1
+    #     )
 
-    import data_schema as ds
-
-    df = pd.read_csv(
-        filepath_or_buffer="filepath",
-        dtype=ds.data_types,
-        na_values=ds.na_values,
-        parse_dates=ds.parse_dates,
-    )
-
-    ListingSchema.validate(df)
-
-    print(df)
+    #NOTE: this is definitely too much for now, I will keep it here commented just in case
