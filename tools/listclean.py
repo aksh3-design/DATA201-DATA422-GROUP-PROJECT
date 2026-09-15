c3b0012d2fe3c3b5f8a709afd2310ea533972beb
anthrpmrphc <anthrpmrphc@gmail.com>
2026-09-16 09:03:17 +1200

    Implemented Listings Data CLeaner:

--- tools/listclean.py
+++ tools/listclean.py
@@ -1,24 +1,77 @@
 import argparse
 import pandas as pd
 from src.main.lib.schema.listings.dtypes import dtypes
+
+import statsmodels.api as sm
+import numpy as np
     
 def clean(data:pd.DataFrame):
 
-    data["neighbourhood"] = data["neighbourhood"].astype("category")
-    
+    initial_rows = data.shape[0]
+
+    print(f"{'num rows':10}|{'removed':10}| log")
+
+    print(f"{data.shape[0]:10}|{data.shape[0]-initial_rows:10}| datetime datatype correction ...")
+
     data["last_review"] = pd.to_datetime(data["last_review"], format="ISO8601")
     
     data["month_year"] = pd.to_datetime(data["month_year"], format="ISO8601")
 
-    data.drop(columns=[
-        "name",
-        "host_id",
-        "host_name",
-        "neighbourhood_group",
-        "last_review",
-        "reviews_per_month",
-        "license"
-    ])
+    print(f"{data.shape[0]:10}|{data.shape[0]-initial_rows:10}| category datatype correction ...")
+
+    data["neighbourhood"] = data["neighbourhood"].astype("category")
+
+    drop_columns = [
+            "name",
+            "host_id",
+            "host_name",
+            "neighbourhood_group",
+            "last_review",
+            "reviews_per_month",
+            "license"
+        ]
+
+    print(f"{data.shape[0]:10}|{data.shape[0]-initial_rows:10}| drop columns:")
+    
+    for col in drop_columns:
+        print(f"{'':10}|{'':10}|\t - '{col}'")
+
+    data.drop(columns=drop_columns, inplace=True)
+
+    print(f"{data.shape[0]:10}|{data.shape[0]-initial_rows:10}| one-hot encoding room_type for use in multiple imputation ...")
+
+    data = pd.concat([data, pd.get_dummies(data[["room_type"]], dtype=int)], axis=1) # one-hot encode roomtype and append for imputation (It makes sense that this would affect the price)
+    data.drop(columns=["room_type_Entire home/apt"], inplace=True) # instead of drop_first=True, for generality
+
+    data.columns = data.columns.str.replace(" ", "_") # statsmodels library does not like spaces in column names
+
+    print(f"{data.shape[0]:10}|{data.shape[0]-initial_rows:10}| multiple imputation to find missing price values ...")
+
+    micedata = sm.MICEData(data[[
+        "room_type_Hotel_room",
+        "room_type_Private_room",
+        "room_type_Shared_room",
+        "price",
+        "number_of_reviews_ltm",
+        "availability_365"
+    ]])
+
+    micedata.update_all(n_iter=10)
+
+    data[[
+            "room_type_Hotel_room",
+            "room_type_Private_room",
+            "room_type_Shared_room",
+            "price",
+            "number_of_reviews_ltm",
+            "availability_365"
+        ]] = micedata.data
+
+    print(f"{data.shape[0]:10}|{data.shape[0]-initial_rows:10}| drop encoded columns")
+
+    data.drop(columns=["room_type_Hotel_room", "room_type_Private_room", "room_type_Shared_room"], inplace=True)
+
+    data = data.astype(dtype=dtypes)
 
     return data
 
@@ -55,4 +108,13 @@ if __name__ == "__main__":
         data.to_csv(f"{default_output}", index=False)
 
     print("cleaning completed.")
+
+
+
+
+
+
+
+
+
 
