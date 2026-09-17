0b5d4c380588cda2830150dbe18171cba1f35b68
anthrpmrphc <anthrpmrphc@gmail.com>
2026-09-18 06:12:01 +1200

    Koordinates Query Code

---
+++ src/tools/koord_get.py
@@ -0,0 +1,128 @@
+import requests
+import json
+from typing import Literal, Self
+
+class VectorResponse():
+
+    def __init__(self, key:str, layer:int, max_results:int=3, radius:int=10000, geometry:Literal["true", "false"]="true", with_field_names:Literal["true", "false"]="true"):
+
+        self.key:str = key
+        self.layer:int = layer
+        self.max_results:int = max_results
+        self.radius:int = radius        
+        self.geometry:str = geometry
+        self.with_field_names:str = with_field_names
+
+        self.response:requests.Response = None
+        self.status:Literal[1, 0, -1] = -1 # Null Status, 1 is a success
+
+    def get_url(self, longitude:float, latitude:float):            
+        return f"https://koordinates.com/services/query/v1/vector.json?key={self.key}&layer={self.layer}&x={longitude}&y={latitude}&max_results={self.max_results}&radius={self.radius}&geometry={self.geometry}&with_field_names={self.with_field_names}"
+
+    def query(self, url:str, max_attempts:int=4):
+
+        if not self.response is None:
+            return self 
+
+        attempts = 0
+
+        while self.response is None:
+
+            response:requests.Response = requests.get(url)
+
+            match response.status_code:
+                case 200:
+                    self.status = 1
+                    self.response = response
+                case 400:
+                    self.status = 1
+                    self.response = response 
+                case 401: #Unauthorised
+                    self.status = 0
+                    print("401 Unauthorized: invalid credentials")
+                case 404: # Not found
+                    self.status = 0
+                    print("404 Not Found")
+                case 429: # too many attempts
+                    self.status = 0
+                    print(f"429 Too Many Attempts: try again {response.headers["Retry-After"]}")
+                case _:
+                    self.status = 0
+                    print(json.dumps(response.json(), indent=4))
+
+            if attempts >= max_attempts:
+                print(f"WARNING Exceeded Max Requests: {max_attempts}")
+                print(f"{url}")
+                self.status = 0
+                return self
+
+        return self
+            
+    def get_code(data:Self):
+        
+        datajson = data.response.json()
+        
+        return datajson["vectorQuery"]["layers"]["123515"]["features"][0]["properties"]["SA22026_V1_00"]
+    
+    def get_name(data:Self):
+    
+        datajson = data.response.json()
+        
+        return datajson["vectorQuery"]["layers"]["123515"]["features"][0]["properties"]["SA22026_V1_00_NAME"]
+
+if __name__ == "__main__":
+
+    query_object = VectorResponse(
+        "59891e1c37c4492eb441d3a962387b22",
+        123515
+    )
+
+    url = query_object.get_url(172.57088, -43.47002)
+
+    name = query_object.query(url).get_name()
+    code = query_object.get_code()
+
+
+# Example 200 Response
+
+# {
+#     "vectorQuery": {
+#         "layers": {
+#             "123515": {
+#                 "type": "FeatureCollection",
+#                 "crs": {
+#                     "type": "name",
+#                     "properties": {
+#                         "name": "EPSG:4326"
+#                     }
+#                 },
+#                 "features": [
+#                     {
+#                         "type": "Feature",
+#                         "properties": {
+#                             "SA22026_V1_00": "321302",
+#                             "SA22026_V1_00_NAME": "Prestons Park",
+#                             "SA22026_V1_00_NAME_ASCII": "Prestons Park",
+#                             "LAND_AREA_SQ_KM": 2.62419668,
+#                             "AREA_SQ_KM": 2.62419668,
+#                             "Shape_Length": 6991.465506881636,
+#                             "Shape_Area": 2624196.6907962393
+#                         },
+#                         "distance": 0,
+#                         "id": 4506
+#                     }
+#                 ],
+#                 "field_names": [
+#                     "SA22026_V1_00",
+#                     "SA22026_V1_00_NAME",
+#                     "SA22026_V1_00_NAME_ASCII",
+#                     "LAND_AREA_SQ_KM",
+#                     "AREA_SQ_KM",
+#                     "Shape_Length",
+#                     "Shape_Area"
+#                 ]
+#             }
+#         }
+#     }
+# }    
+
