ce5696f5ccebde45c39f4ab9c7a87d39a1ffe5ad
anthrpmrphc <anthrpmrphc@gmail.com>
2026-09-23 12:30:10 +1200

    refactoring: bondclean.py - abstracted pandas dataframe transformations into helper functions. - abstracted logging print statements into helper functions. - project file rearrangement. -  added data/ and out/ directories for raw/processed data.

---
+++ src/bondclean.py
@@ -0,0 +1,243 @@
+# Tenancy Bonds Dataset Cleaning Utility.
+# More information in docs/bondclean.md
+
+from src.main.lib.schema.bonds.dtypes import dtypes
+from main.lib.transform import filter_row_by_value, parse_column_entries, to_numerical_specific
+from main.lib.log import print_clean_log, print_clean_cascade, print_clean_simple
+
+import pandas as pd
+import statsmodels.api as sm
+
+import json
+import configparser
+
+# gather configurations into global variables
+
+def get_SA22019_TA2019_WARD2019_table(filepath:str="src/data/SA22019_TA2019_WARD2019.json"):
+    """Generates lookup table to parse SA22019 codes to TA2019 and WARD2019 codes and names"""
+    with open(filepath, 'r', encoding='utf-8') as file:
+        
+        results = json.load(file)
+
+    return results
+
+def get_daterange_from_config(filepath:str="src/data/data_range.ini"):
+    """Loades data date ranges from configuration files"""
+    config = configparser.ConfigParser()
+
+    config.read(filepath)
+
+    return ([config["daterange"]["start_date"]], [config["daterange"]["end_date"]])
+
+SA22019_TABLE = get_SA22019_TA2019_WARD2019_table()
+START_DATE, END_DATE = get_daterange_from_config() 
+
+# clean the dataset
+
+def clean(data:pd.DataFrame):
+
+    initial_rows = data.shape[0]
+
+    drop_columns = [ # remove for ease of MICE imputation of Log Std Dev, Location Id, Geometric Mean Rent
+            "Upper Quartile Rent",
+            "Lower Quartile Rent",
+            "Median Rent"
+            ]
+    
+    mice_columns = {
+            "Log Std Dev Weekly Rent": float,
+            "Geometric Mean Rent": float,
+            "Total Bonds": int,
+            "Active Bonds": int,
+            "Closed Bonds": int
+            }
+
+    # drop unnecessary columns =======================================================
+
+    print_clean_log(data, initial_rows, "log")
+
+    print_clean_log(data, initial_rows, "drop columns:")
+    print_clean_cascade(drop_columns)
+    
+    data = data.drop(columns=drop_columns)
+    
+    # convert TimeFrame to datetime datatype =========================================
+
+    print_clean_log(data, initial_rows, "datetime datatype correction ...")
+    data["TimeFrame"] = pd.to_datetime(data["TimeFrame"], format="ISO8601")
+
+    # remove -99 + NULL ==============================================================
+
+    print_clean_log(data, initial_rows, "removing invalid -99 location codes ...")
+    data = filter_row_by_value(data, "Location Id", "-99")
+
+    print_clean_log(data, initial_rows, "removing invalid NULL location codes ...")
+    data = filter_row_by_value(data, "Location Id", "NULL")
+
+    # remove missing location codes ==================================================
+
+    print_clean_log(data, initial_rows, "removing missing location codes ...")
+    
+    data = data.dropna(subset=["Location Id"])
+    data["Location Id"] = data["Location Id"].astype("int64")
+
+    # parse functions ================================================================
+
+    print_clean_log(data, initial_rows, "parsing TA2019 and WARD2019 location names from SA2-2019 'Location Id' ...")
+
+    data = parse_column_entries(data, "Location Id", parse_TA2019, True, "TA2019")
+    data = parse_column_entries(data, "Location Id", parse_WARD2019, True, "WARD2019")
+
+    print_clean_log(data, initial_rows, "parsing Number of Beds Categories ...")
+
+    data = parse_column_entries(data, "Number Of Beds", parse_num_beds)
+
+    # convert datatypes of columns to suitable type for mice =========================
+
+    print_clean_log(data, initial_rows, "preparing columns dtypes for multiple imputation ...")
+    print_clean_cascade(mice_columns)
+
+    for column, type in mice_columns.items():
+        data = to_numerical_specific(data, column, type)
+
+    # convert column names to suitable format for statsmodel library =================
+
+    temp_mice_column_names = []
+
+    for column in mice_columns.keys():
+        temp_mice_column_names.append(column.replace(" ", "_"))
+    
+    data.columns = data.columns.str.replace(" ", "_") 
+
+    # run mice on columns ============================================================
+
+    print_clean_log(data, initial_rows, "multiple imputation on remaining columns ...")
+    print_clean_cascade(mice_columns.keys())
+
+    mice_data = sm.MICEData(data[temp_mice_column_names])
+    mice_data.update_all(n_iter=10) # recommended iteration count from lectures
+
+    # cleanup column names, column datatypes =========================================
+
+    print_clean_log(data, initial_rows, "imputations complete!")
+
+    data[temp_mice_column_names] = mice_data.data
+    data.columns = data.columns.str.replace("_", " ")
+
+    data = data.astype(dtype=dtypes)
+
+    # filter for entries with daterange (see date_range.ini) =========================
+
+    print_clean_log(data, initial_rows, f"filtering for date range {START_DATE} - {END_DATE} ...")
+
+    data = data[data["TimeFrame"].between(START_DATE[0], END_DATE[0])]
+
+    # filter for entrise in Christchurch City ========================================
+
+    print_clean_log(data, initial_rows, "Removing rows not in Christchurch City")
+
+    data = filter_row_by_value(data, "TA2019", "Christchurch City")
+
+    print_clean_log(data, initial_rows, "Cleanup Complete.")
+
+    return data
+
+def parse_TA2019(code:int|str):
+    """
+    Takes SA2-2019 location codes and converts them into TA2019 names.
+
+    Args:
+        code (int): SA2-2019 area code
+    Returns:
+            str: TA2019 name. 
+    """
+
+    try:
+        result = SA22019_TABLE[str(code)]["TA2019_name"]
+    except KeyError:
+        result = code
+
+    return result
+
+def parse_WARD2019(code:int|str):
+    """
+    Takes SA2-2019 location codes and converts them into WARD2019 names.
+
+    Args:
+        code (int): SA2-2019 area code
+    Returns:
+            str: WARD2019 name. 
+    """
+    
+    try:
+        result = SA22019_TABLE[str(code)]["WARD2019_name"]
+    except KeyError:
+        result = code
+    
+    return result
+
+def parse_num_beds(val:str):
+    """
+    Takes string type entries from 'Number Of Beds' column and parses a to-spec categorical string classifcation.
+
+    Args:
+        val (str): entry (Number Of Beds)
+
+    Returns:
+        str: categorical classification
+    """
+
+    val = str(val)
+
+    match val:
+        case "1":
+            return val
+        case "2":
+            return val
+        case "3":
+            return val
+        case "4":
+            return val
+        case "5":
+            return "5+"
+        case "5+":
+            return val
+        case "NA":
+            return val
+        case "ALL":
+            return val
+
+    # handle rouge numbers
+
+    if int(val) > 5:
+        return "5+"
+    if int(val) < 1:
+        return "NA"
+
+if __name__ == "__main__":
+
+    from pathlib import Path
+
+    filename = "Detailed-Quarterly-Tenancy-Q1-2020-Q3-2026.csv"
+    filepath = Path(f"./data/{filename}")
+    fileout = Path(f"./out/{filename}")
+
+    data = None
+
+    print(f"{'num rows':10}|{'removed':10}| log")
+
+    try:
+        print_clean_simple("loading csv ...")
+        data = pd.read_csv(filepath, dtype={"Number Of Beds" : "string"}, keep_default_na=False) # NA is a category, not null
+    except FileNotFoundError:
+        print_clean_simple(f"No such file or directory: '{filepath}'")
+        exit()
+
+    print_clean_simple("cleaning csv ...")
+    data = clean(data)
+
+    print_clean_simple("writing csv ...")
+    data.to_csv(f"{fileout}", index=False)
+
+    print_clean_simple("cleaning completed.")
+
---
+++ src/main/lib/log.py
@@ -0,0 +1,27 @@
+# Collection of basic print-log helper functions
+import pandas as pd
+
+def print_clean_simple(message:str):
+    print(f"{' ':10}|{' ':10}| {message}")
+
+def print_clean_log(data:pd.DataFrame, initial_rows:int, message:str):
+    """prints a simple log message, detailing the number of rows
+
+    Args:
+        data (pd.DataFrame): _description_
+        message (str): _description_
+    """
+    print(f"{data.shape[0]:10}|{initial_rows-data.shape[0]:10}| {message}")
+
+def print_clean_cascade(messages:list[str], indent=4):
+    """prints a cascade of logs, identical formatting to print_log, without information
+    on number of rows left/removed.
+
+    Args:
+        messages (list[str]): messages to cascade
+        indent (int, optional): Number of ' ' whitespace characters to indent log message by. Defaults to 4.
+    """
+    for message in messages:
+        print(f"{'':10}|{'':10}|{indent*' '}- {message}")
+
+print_clean_log.calls = 0
